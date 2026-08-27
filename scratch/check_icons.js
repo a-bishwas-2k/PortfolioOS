a8d7ca2ef9470ba49cc8d4bf30cbde3401b6d02b
Abhishek Biswas <21053421@kiit.ac.in>
2026-08-27 10:01:58 +0530

    Remove unnecessary scratch and archive folders

--- scratch/check_icons.js
+++
@@ -1,9 +0,0 @@
-const https = require('https');
-
-const slugs = ['java', 'oracle', 'openjdk', 'csharp', 'css3', 'framer', 'framermotion', 'amazonaws', 'aws'];
-
-slugs.forEach(slug => {
-  https.get(`https://cdn.simpleicons.org/${slug}`, (res) => {
-    console.log(`${slug}: ${res.statusCode}`);
-  });
-});
